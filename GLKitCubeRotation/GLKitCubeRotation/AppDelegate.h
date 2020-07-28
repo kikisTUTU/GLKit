f49e9998b5bfc3e2d7dd738214fd1d2698f2f67d
图图 <tutu@tutudeMacBook-Pro.local>
2020-07-28 17:29:51 +0800

    first

---
+++ GLKitCubeRotation/GLKitCubeRotation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GLKitCubeRotation
+//
+//  Created by 图图 on 2020/7/28.
+//  Copyright © 2020 图图. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
